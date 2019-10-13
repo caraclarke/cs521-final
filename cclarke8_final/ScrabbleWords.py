ac31f957275f57c3208443e1706924190f9e6400
caraclarke <cara.clarke8@gmail.com>
2019-10-13 12:07:30 -0400

    make it so

---
+++ cclarke8_final/ScrabbleWords.py
@@ -0,0 +1,10 @@
+class ScrabbleWords():
+    def __init__(self, l_one, l_two, l_three, l_four, l_five, l_six, l_seven):
+        # you can have up to seven letters per user
+        self.__l_one = l_one
+        self.__l_two = l_two
+        self.__l_three = l_three
+        self.__l_four = l_four
+        self.__l_five = l_five
+        self.__l_six = l_six
+        self.__l_seven = l_seven
