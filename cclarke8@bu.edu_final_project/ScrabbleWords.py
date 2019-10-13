374b56b5412f2a145e5fcdfef6cf60de5b605f3e
caraclarke <cara.clarke8@gmail.com>
2019-10-13 18:21:57 -0400

    add doc add repr

--- cclarke8@bu.edu_final_project/ScrabbleWords.py
+++ cclarke8@bu.edu_final_project/ScrabbleWords.py
@@ -5,34 +5,41 @@ from WordScores import WordScores
 class ScrabbleWords(WordScores):
     def __init__(self, user_string):
         super().__init__()
-        # merged string of letters available to user
-        self.__user_string = user_string
-        self.__counted_string = Counter(user_string)
-        self.__words = self.get_words_from_dictionary()
+        # public attributes
+        self.user_string = user_string  # merged string of letters available to user
+        self.counted_string = Counter(user_string)
+        # private attribute
+        # maybe another private attr with scores per letter
+        self.__words = self.__get_words_from_dictionary()
 
     # Accessors
     def get_user_string(self):
-        return self.__user_string
+        return self.user_string
 
     def get_counted_string(self):
-        return self.__counted_string
+        return self.counted_string
 
     def get_words(self):
         return self.__words
 
     # Mutators
     def set_user_string(self, user_string):
-        self.__user_string = user_string
+        self.user_string = user_string
 
     def set_counted_string(self, counted_string):  # should we be setting this?
-        self.__counted_string = counted_string
+        self.counted_string = counted_string
 
     def set_words(self, words):
         self.__words = words
 
+    # Description: override repr to return letter count of user provided string
+    # Returns: count of letters provided by user
+    def __repr__(self):
+        return repr(self.counted_string)
+
     # Description: read txt file and create word dictionary
     # Returns list with tuple containing word and count of each letter in word
-    def get_words_from_dictionary(self):
+    def __get_words_from_dictionary(self):
         with open('dictionary.txt') as dictionary:
             lines = (word.strip().lower() for word in dictionary)
             # list with word and count of each letter in word
@@ -48,7 +55,7 @@ class ScrabbleWords(WordScores):
         for new_word, word_length in self.__words:
             # If the suggested word is at least 2 letters (so it can be played on the board)
             # and if there is nothing left after you subtract word_length from the count of user provided letters
-            if len(new_word) >= 2 and not (word_length - self.__counted_string):
+            if len(new_word) >= 2 and not (word_length - self.counted_string):
                 word_score = self.get_word_score(new_word)
                 # Add tuple with word and score to list to be returned
                 potential_words.append((new_word, word_score))
