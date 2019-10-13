724dee347318ff15ea8fa2b4aeecd37bd70e9584
caraclarke <cara.clarke8@gmail.com>
2019-10-13 13:12:22 -0400

    add comments with project plan

--- cclarke8_final/word_finder.py
+++ cclarke8_final/word_finder.py
@@ -4,6 +4,17 @@ Main file to run the word finder program
 import sys
 from ScrabbleWords import ScrabbleWords
 
+# Prompt user to enter their scrabble letters
+# Match letters to point values
+# Obtain potential words from letters and return to user
+# Next to words show point raiting
+# Order words by highest point rating
+
+# Have user input what word they chose, update their score
+# Can have up to 4 users playing (nice to have unlimited)
+# Cycle through endlessly until they choose 'game over'
+# Can skip users turn if they dont want to use word finder and just update their scores
+
 
 def main():
     # switch this to be from user and try/except
