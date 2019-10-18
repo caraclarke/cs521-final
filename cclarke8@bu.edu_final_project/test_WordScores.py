559e108a61d0ef7a97cc0ea05be950e6a7e2ea71
caraclarke <cara.clarke8@gmail.com>
2019-10-18 19:43:06 -0400

    use unittest instead

--- cclarke8@bu.edu_final_project/test_WordScores.py
+++ cclarke8@bu.edu_final_project/test_WordScores.py
@@ -1,4 +1,4 @@
-# import class to test
+import unittest
 from WordScores import WordScores, WORD_SCORES_DICT
 
 # mock dictionary of scores to test mutators
@@ -7,17 +7,23 @@ MOCK_SCORES = {'a': 22, 'z': 47}
 # Test WordScores Class
 
 
-class TestWordScores():
-    def setup(self):
-        self.word_scores = WordScores()
-
+class TestWordScores(unittest.TestCase):
     def test_returns_letter_scores(self):
-        assert self.word_scores.get_letter_scores() == WORD_SCORES_DICT
+        self.word_scores = WordScores()
+        scores = self.word_scores.get_letter_scores()
+        self.assertEqual(scores, WORD_SCORES_DICT)
 
     def test_can_update_letter_scores(self):
+        self.word_scores = WordScores()
         self.word_scores.set_letter_scores(MOCK_SCORES)
-        assert self.word_scores.get_letter_scores() == MOCK_SCORES
+        new_scores = self.word_scores.get_letter_scores()
+        self.assertEqual(new_scores, MOCK_SCORES)
 
     def test_get_word_score(self):
-        assert self.word_scores.get_word_score(
-            'python') == 14  # score for word 'python'
+        self.word_scores = WordScores()
+        test_word_score = self.word_scores.get_word_score('python')
+        self.assertEqual(test_word_score, 14)  # score for word 'python'
+
+
+if __name__ == '__main__':
+    unittest.main()
