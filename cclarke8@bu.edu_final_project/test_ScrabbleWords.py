0f41fdb938c8ae961ba431f48d3acd7a10f4ae16
caraclarke <cara.clarke8@gmail.com>
2019-10-18 20:02:15 -0400

    test scrabblewords

--- cclarke8@bu.edu_final_project/test_ScrabbleWords.py
+++ cclarke8@bu.edu_final_project/test_ScrabbleWords.py
@@ -2,6 +2,44 @@ import unittest
 from collections import Counter
 from ScrabbleWords import ScrabbleWords
 
+# list of words suggested from test string 'python'
+WORDS_FROM_PYTHON = [('to', 2),
+                     ('on', 2),
+                     ('no', 2),
+                     ('nt', 2),
+                     ('tn', 2),
+                     ('ot', 2),
+                     ('not', 3),
+                     ('ton', 3),
+                     ('po', 4),
+                     ('op', 4),
+                     ('pt', 4),
+                     ('np', 4),
+                     ('tp', 4),
+                     ('pn', 4),
+                     ('top', 5),
+                     ('oh', 5),
+                     ('ny', 5),
+                     ('th', 5),
+                     ('nh', 5),
+                     ('pot', 5),
+                     ('ho', 5),
+                     ('opt', 5),
+                     ('yo', 5),
+                     ('yn', 5),
+                     ('ht', 5),
+                     ('ty', 5),
+                     ('hot', 6),
+                     ('toy', 6),
+                     ('hon', 6),
+                     ('hp', 7),
+                     ('tony', 7),
+                     ('ph', 7),
+                     ('hop', 8),
+                     ('pty', 8),
+                     ('thy', 9),
+                     ('python', 14)]
+
 '''
 Description: Helper function to get words from dictionary file
 Returns: words from dictionary.txt
@@ -59,6 +97,11 @@ class TestScrabbleWords(unittest.TestCase):
         test_repr = self.scrabble_word.__repr__()
         test_counter = Counter('python')
         self.assertEqual(test_repr, str(test_counter))
+
+    def test_get_words_list(self):
+        self.scrabble_word = ScrabbleWords('python')
+        suggested_words = self.scrabble_word.get_words_list()
+        self.assertEqual(suggested_words, WORDS_FROM_PYTHON)
 
 
 if __name__ == '__main__':
