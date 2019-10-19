d02e07c2880f0f4f9022cb26ee65f58d7453ac66
caraclarke <cara.clarke8@gmail.com>
2019-10-18 20:42:15 -0400

    add some helper functions and more conversions

--- cclarke8@bu.edu_final_project/word_finder.py
+++ cclarke8@bu.edu_final_project/word_finder.py
@@ -5,14 +5,29 @@ import sys
 from ScrabbleWords import ScrabbleWords
 
 '''
-Description: Starts function to allow user to input letters
-Returns: Formatted list of suggested words and scores
+Description: Prints list of words out for user
+Returns: None
 '''
 
 
-def main():
-    # switch this to be from user and try/except
-    user_input = input('Enter all available letters for your turn: ')
+def print_words(words):
+    for word in words:
+        print("""Suggested word: {}
+        Potential score: {}
+        ________________________________________
+        """.format(*word))
+
+
+'''
+Description: Gets user input for letters and checks for potential matches
+index: current player, default 1
+Returns: List of potential words
+'''
+
+
+def play_round(index=1):
+    user_input = input(
+        'Player {}, Enter all available letters for your turn: '.format(index))
     for value in user_input:
         try:  # Check user didn't enter numbers
             if value.isdigit():
@@ -22,15 +37,41 @@ def main():
             sys.exit()
         else:
             user_words = ScrabbleWords(user_input)
+            return user_words
 
-    # get potential words from class
-    sorted_words = user_words.get_words_list()
-    # format nicely and display to user
-    for word in sorted_words:
-        print("""Suggested word: {}
-Potential score: {}
-________________________________________
-        """.format(*word))
+
+'''
+Description: Starts function to allow user to input letters
+Returns: Formatted list of suggested words and scores
+'''
+
+
+def main():
+    # switch this to be from user and try/except
+    player_input = input("How many players are playing today? ")
+    try:
+        player_count = int(player_input)
+    except ValueError:  # if they dont enter a number show error and exit
+        print("Please enter only numbers. Try again.")
+        sys.exit()
+    except:  # something else went wrong, show generic error and exit
+        print("Something went wrong. Try again.")
+        sys.exit()
+
+    # Loop through players if more than one
+    if (player_count > 1):
+        # loop through all the players
+        for player, index in enumerate(range(player_count)):
+            user_words = play_round(index)
+            # get potential words from class
+            sorted_words = user_words.get_words_list()
+            # format nicely and display to user
+            print_words(sorted_words)
+    else:  # Otherwise just call once
+        user_words = play_round()
+        sorted_words = user_words.get_words_list()
+        # format nicely and display to user
+        print_words(sorted_words)
 
 
 if __name__ == '__main__':
